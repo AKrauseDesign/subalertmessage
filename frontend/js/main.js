faa12c45e8e28759628540c0ca8605f5fedcf009
Styler <stylergaminghd@gmail.com>
2015-07-31 18:01:00 +0200

    V-Dank

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3100');
+var socket = io('http://hosted.stylerdev.io:3100');
 
 var queue = [];
 
@@ -13,15 +13,18 @@ var displayTime = 10 * 1000;
 //   console.log(displayTime);
 // });
 var playing = false;
-
 var notify = function(data) {
   if(data !== null) {
     queue.push(data);
   }
   if(!playing) {
     playing = true;
+    var msg = queue[0].message;
     messageEl.textContent = queue[0].message;
     usernameEl.textContent = queue[0].username;
+    setTimeout(function () {
+      responsiveVoice.speak(msg, "US English Female", {rate: 0.8});
+    }, 2000);
     queue.shift();
     sound.play();
     container.classList.add('visible');
