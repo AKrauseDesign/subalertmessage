8c9da4f5c643bb0d03e9e3eb13df751aae0322b4
TimohStudios <timogos.k@live.nl>
2015-08-01 22:56:52 +0200

    Fixit! Fix rendering, order and removing old

--- frontend/dashboard/js/dashboard.js
+++ frontend/dashboard/js/dashboard.js
@@ -4,7 +4,7 @@ var lastTen = [];
 var list = document.getElementsByClassName('content')[0];
 
 var sectionFactory = function(id, username, message) {
-  list.insertAdjacentHTML('afterbegin', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + '</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> </div> </section>');
+  list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + '</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> </div> </section>');
 };
 
 var findLastTen = function(data) {
@@ -12,6 +12,7 @@ var findLastTen = function(data) {
   if(lastTen.length > 10) {
     lastTen.pop();
    }
+  list.innerHTML = ' ';
   for(var j = 0; j < lastTen.length; j++) {
     sectionFactory(j+1, lastTen[j].username, lastTen[j].message);
   }
