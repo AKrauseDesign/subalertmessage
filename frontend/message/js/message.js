68a9cd2b4084360346104aff71f6ec45619846f3
TimohStudios <timogos.k@live.nl>
2015-08-01 23:23:32 +0200

    Untested queue fix. Changed initial state for dash too

--- frontend/message/js/message.js
+++ frontend/message/js/message.js
@@ -2,45 +2,47 @@ var socket = io('http://localhost:3100');
 var queue  = [];
 
 var sound       = document.querySelector('.sound');
+var tts         = document.querySelector('.tts');
 var container   = document.querySelector('.subalert');
 var usernameEl  = container.querySelector('.username');
 var messageEl   = container.querySelector('.message');
 var displayTime = 5 * 1000;
+var ttsTime     = 15 * 1000;
 var lastTen     = [];
 var playing     = false;
 var list        = document.getElementByClassName('content');
 
-
 var notify = function(data) {
-  if(data !== null) {
-    queue.push(data);
-  }
+  queue.push(data);
   if(!playing) {
     playing = true;
-    var tts = 'http://text-to-speech-demo.mybluemix.net/synthesize?voice=en-US_AllisonVoice&text=';
-    // var tts = 'http://hosted.stylerdev.io:3100/synthesize?voice=en-US_AllisonVoice&text=';
-    var msg                = queue[0].message;
-    messageEl.textContent  = queue[0].message;
+    var ttsUrl = 'http://text-to-speech-demo.mybluemix.net/synthesize?voice=en-US_AllisonVoice&text=';
+    // var ttsUrl = 'http://hosted.stylerdev.io:3100/synthesize?voice=en-US_AllisonVoice&text=';
+    var msg = queue[0].message;
+    messageEl.textContent = msg;
     usernameEl.textContent = queue[0].username;
-    setTimeout(function () {
-      $('.tts').attr('src',  tts + encodeURIComponent(msg));
-    }, 2000);
     queue.shift();
     container.classList.add('visible');
-    setTimeout(function(){
-      container.classList.remove('visible');
+    tts.addEventListener('loadedmetadata', function(){
+      ttsTime = tts.duration * 1000;
+      console.log(ttsTime);
+    });
+    setTimeout(function() {
+      tts.src = ttsUrl + encodeURIComponent(msg);
       setTimeout(function() {
-        playing = false;
-        if(queue.length > 0) {
-          notify(null);
-        }
-      }, 3000);
-    }, displayTime);
+        container.classList.remove('visible');
+        setTimeout(function() {
+          playing = false;
+          if(queue.length > 0) {
+            notify(null);
+          }
+        }, ttsTime + 2000);
+      }, displayTime - 2000);
+    }, 2000);
   }
 };
 
-socket.on('subMsg', lastTen);
 socket.on('subMsg', notify);
 socket.on('stopSound', function(){
-  $('.tts').attr('src', '');
+  tts.stop();
 });
