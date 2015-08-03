68b8d659c57248dcee3f6f11463448102fc140ca
Maximilian Lloyd <maxlloyd2000@hotmail.com>
2015-08-03 13:58:42 +0200

    new style implemented, working on injecting data

--- frontend/dashboard/js/dashboard.js
+++ frontend/dashboard/js/dashboard.js
@@ -3,9 +3,9 @@ var socket = io('http://localhost:3100');
 var lastTen = [];
 var list = document.getElementsByClassName('content')[0];
 
-var sectionFactory = function(id, username, message, resub) {
-  if(resub > 0) list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + '</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> <span> Thanks For Resubscribing For' +  resub + ' in a row! </span> </div> </section>');
-  else list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + '</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> </div> </section>');
+var sectionFactory = function(username, message, resub) {
+  if(resub > 0) list.insertAdjacentHTML('beforeend', '<div class=\"cart\"><div class=\"img-hold\"> img link </div><div class =\"message-wrap\"><div class =\"user-info\"> <span class=\"username\">' + username + ' (resub ' + resub + 'months)</span><span class=\"time\"> ' + Date.getDate() + '/' + Date.getMonth() + '/'  + Date.getFullYear() + '</span></div></div></div> <span class=\"message\">' + message + '</span></div></section>');
+  else ist.insertAdjacentHTML('beforeend', '<div class=\"cart\"><div class=\"img-hold\"> img link </div><div class =\"message-wrap\"><div class =\"user-info\"> <span class=\"username\">' + username + '(New Subscriber)</span><span class=\"time\"> 15 minutes ago </div><span class=\"date\"> 0.1.08.2015 </span></div></div> <span class=\"message\">' + message + '</span></div></section>')
 };
 
 var findLastTen = function(data) {
@@ -16,9 +16,11 @@ var findLastTen = function(data) {
    }
   list.innerHTML = ' ';
   for(var j = 0; j < lastTen.length; j++) {
-    sectionFactory(j+1, lastTen[j].username, lastTen[j].message, lastTen[j].resub);
+    sectionFactory(lastTen[j].username, lastTen[j].message, lastTen[j].resub);
   }
 }
 };
+
+sectionFactory('test', 'test', 5);
 
 socket.on('subMsg', findLastTen);
