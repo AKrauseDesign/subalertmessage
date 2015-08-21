206bce6e09ebbabb7bec4e292f2d72546a2f9608
Styler <stylergaminghd@gmail.com>
2015-08-21 15:49:13 +0200

    fixerino

--- frontend/dashboard/js/dashboard.js
+++ frontend/dashboard/js/dashboard.js
@@ -3,10 +3,6 @@ var socket = io('http://hosted.stylerdev.io:3100');
 var lastTen = [];
 var list = document.getElementsByClassName('content')[0];
 
-var sectionFactory = function(id, username, message, resub) {
-  if(resub > 0) list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username+' (Resub: '+resub+' months)'+'</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span></section>');
-  else list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + ' (New Subscriber)</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> </div> </section>');
-};
 var sectionFactory = function(username, message, resub) {
   var date = new Date();
   if(resub > 0)
