8e4bcae287a930bad7245b413b10d91ec89a67f6
Styler <stylergaminghd@gmail.com>
2015-08-21 15:47:45 +0200

    fixerino

--- frontend/dashboard/js/dashboard.js
+++ frontend/dashboard/js/dashboard.js
@@ -3,17 +3,15 @@ var socket = io('http://hosted.stylerdev.io:3100');
 var lastTen = [];
 var list = document.getElementsByClassName('content')[0];
 
-<<<<<<< HEAD
 var sectionFactory = function(id, username, message, resub) {
   if(resub > 0) list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username+' (Resub: '+resub+' months)'+'</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span></section>');
   else list.insertAdjacentHTML('beforeend', '<section id=\"' + id + '\"><span class=\"number\">' + id + '</span><div class=\"wrap\"><span class=\"title\">Username: </span> <span class=\"username\">' + username + ' (New Subscriber)</span> <br> <span class=\"title\">Message: </span> <span class=\"message\">' + message + '</span> </div> </section>');
-=======
+};
 var sectionFactory = function(username, message, resub) {
   var date = new Date();
   if(resub > 0)
   list.insertAdjacentHTML('beforeend', '<div class=\"cart\"><div class=\"img-hold\"> img link </div><section class =\"message-wrap\"><div class =\"user-info\"> <span class=\"username\">' + username + ' (Resub for ' + resub + ' months)</span><span class=\"time\">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</span></div><p class=\"message\">' + message + '</p></div></div></section></div>');
   else list.insertAdjacentHTML('beforeend', '<div class=\"cart\"><div class=\"img-hold\"> img link </div><section class =\"message-wrap\"><div class =\"user-info\"> <span class=\"username\">' + username + ' (New Subscriber)</span><span class=\"time\">' + date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + '</span></div><p class=\"message\">' + message + '</p></div></div></section></div>');
->>>>>>> 82f8a7a946ee9828720b66ff92c31f0d19600a8c
 };
 
 var findLastTen = function(data) {
