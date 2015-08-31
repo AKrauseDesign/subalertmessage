0b0afaa7fb3cdf0a4f39e4035c4fbcff84a50b71
Max <me@maxlloyd.no>
2015-08-31 21:34:34 +0200

    browser-sync

--- frontend/dashboard/gulpfile.js
+++ frontend/dashboard/gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/*.sass')
@@ -23,6 +24,13 @@ gulp.task('js', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('browser-sync', function() {
+    browserSync.init(['dist/*.css', 'dist/*.js', './index.html'], {
+      server: {
+        baseDir: './'
+      }
+    });
+});
 
 gulp.task('watch', function() {
    gulp.watch('./sass/main.sass', ['sass']);
@@ -33,7 +41,7 @@ gulp.task('watch', function() {
    gulp.watch(['dist/**', 'index.html']).on('change', livereload.changed);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'browser-sync']);
 
 function errorAlert(error){
   notify.onError({title: "Gulp Error", message: "Beep beep beep, stuffs going down. Check the console."})(error); //Error Notification
